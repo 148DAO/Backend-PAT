6bc768eb84f52d895492003432c1ceb7096e1b00
Melvin5585 <mbhoni99@gmail.com>
2024-08-03 21:22:19 +0200

    endpoints error handling

--- app/crud/user.py
+++ app/crud/user.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from app.db.models import User
 from app.schemas.user import UserCreate
 
-def create_user(db: Session, user: UserCreate):
+def create_new_user(db: Session, user: UserCreate):
     db_user = User(**user.model_dump())
     db.add(db_user)
     db.commit()
